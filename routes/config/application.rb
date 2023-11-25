a12e038a65fee1420390b2c0d926eed771de6f30
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-24 19:46:54 -0500

    Finish 00

--- routes/config/application.rb
+++ routes/config/application.rb
@@ -22,7 +22,7 @@ module Routes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-
+    config.action_controller.default_protect_from_forgery = false
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
